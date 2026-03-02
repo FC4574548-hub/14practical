46149efdf612e6350f1812d2afa182c58c505dcd
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-02 15:18:26 +0200

    Update chainedHash.java

--- chainedHash.java
+++ chainedHash.java
@@ -1,1 +1,50 @@
+class Node {
+    String key;
+    String value;
+    Node next;
+    
+    public Node (String k, String v) {
+        key = k;
+        value = v;
+    }
+}
+
+public class chainedHash {
+
+    private Node[] table;
+    private int m;
+    
+    public chainedHash(int size) {
+        
+        m = size;
+        table = new Node[m + 1];
+    }
+    private int hash(String key) {
+        int x = Integer.parseInt(key);
+        return (x % m) + 1;
+    }
+    public void insert(String key, String value) {
+        int i = hash(key);
+        Node head = table[i];
+        Node current = head;
+        
+        while(current != null){
+            if(current.key.equals(key)){
+                current.value = value;
+                return;
+            }
+            current= current.next;
+        }
+        Node newN = new Node(key, value);
+        newN.next = head;
+        table[i] = newN;
+        
+        while(current!= null){
+            if(current.key.equals(key))
+                return current.value;
+            current = current.next;
+        }
+        return null;
+    }
+}
 
